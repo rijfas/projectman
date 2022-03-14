b2ab3282bfea64d768084081f63c2bea7eaa2160
FaizArifUP <faizarif8872@gmail.com>
2022-03-14 15:45:07 +0530

    feat: employee attendance

--- attendance/views.py
+++ attendance/views.py
@@ -16,7 +16,11 @@ def create_attendance(request):
     return render(request, 'attendance/create.html', {'form': form})
 
 def list_attendance(request):
-    attendances = Attendance.objects.filter(manager=request.user)
+    if request.user.is_manager:
+        attendances = Attendance.objects.filter(manager=request.user)
+    else:
+        attendances = Attendance.objects.filter(employee__user=request.user)
+    print(Attendance.objects.all())
     return render(request, 'attendance/list.html', {'attendances': attendances})
 
 
@@ -27,3 +31,9 @@ def delete_attendance(request, pk):
         return redirect('attendance:list')
     context = {'attendance': attendance}
     return render(request, 'attendance/delete.html', context=context)
+
+def mark_attendance(request, id):
+    attendance = Attendance.objects.get(id=id)
+    attendance.marked = True
+    attendance.save()
+    return redirect('attendance:list')
